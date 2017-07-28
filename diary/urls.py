f80faf222fa27989a6cc87fb0c248b1042792f28
junjk <jkjun2091@snu.ac.kr>
2017-07-28 20:01:32 +0900

    다이어리 검색구현, 질문 내용검색 구현

--- diary/urls.py
+++ diary/urls.py
@@ -4,7 +4,8 @@ from diary import views
 
 urlpatterns = [
     url(r'^$', views.diary_list, name='diary_list'),
-    url(r'^new$', views.diary_new, name='diary_new'),
+    url(r'^search/$',views.diary_search, name='diary_search'),
+    url(r'^new/$', views.diary_new, name='diary_new'),
     url(r'^(?P<pk>\d+)/detail/$', views.diary_detail, name='diary_detail'),
     url(r'^(?P<pk>\d+)/edit/$', views.diary_edit, name='diary_edit'),
     url(r'^(?P<pk>\d+)/del/$', views.diary_delete, name='diary_delete'),
--- qna/urls.py
+++ qna/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     url(r'^main/$',views.main,name='main'),
     url(r'^search/$',views.question_search,name='question_search'),
     url(r'^search_day$',views.question_search_day, name='question_search_day'),
+    url(r'^search_content$',views.question_search_content,name='question_search_content'),
     url(r'^other/$', views.other_people, name='other_people'),
     url(r'^(?P<question_id>\d+)/detail/$',views.question_detail,name='question_detail'),
     url(r'^(?P<answer_id>\d+)/edit/$',views.question_edit,name='question_edit'),
