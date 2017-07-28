8448fb5c2171712fe746f9d44846a3eafefd2005
gustos <jucie15@nate.com>
2017-07-28 13:01:57 +0900

    merged

--- qna/middleware.py
+++ qna/middleware.py
@@ -6,6 +6,7 @@ from django.shortcuts import redirect, render
 
 class QuestionMiddleware(object):
     def __init__(self, get_response):
+        # 미들웨어 초기화 함수, get_response 외에 인자를 받을 수 없다.
         self.get_response = get_response
         # One-time configuration and initialization.
 
@@ -23,6 +24,7 @@ class QuestionMiddleware(object):
         return
 
     def process_view(self, request, view_func, view_args, view_kwargs):
+        # 장고의 view가 호출되기 전에 실행된다.
         print(request.path)
         allow_url = [
             '/qna',
@@ -36,6 +38,7 @@ class QuestionMiddleware(object):
         #                 return redirect('qna:question')
 
     def process_template_response(self, request, response):
+        # 장고의 view가 실행이 끝난 후 실행된다.
         return response
 
 
